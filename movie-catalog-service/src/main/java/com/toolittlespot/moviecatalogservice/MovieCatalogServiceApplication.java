65b521bd68a00dc3214b738e4c50176cc10db91f
Igor K <russdreamer@gmail.com>
2019-09-11 15:37:10 +0300

    add timeout for the fault tolerance

--- movie-catalog-service/src/main/java/com/toolittlespot/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/toolittlespot/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -13,7 +14,11 @@ public class MovieCatalogServiceApplication {
 	@Bean
     @LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		factory.setConnectTimeout(3000);
+		factory.setReadTimeout(3000);
+		factory.setConnectionRequestTimeout(3000);
+		return new RestTemplate(factory);
 	}
 
 	@Bean
