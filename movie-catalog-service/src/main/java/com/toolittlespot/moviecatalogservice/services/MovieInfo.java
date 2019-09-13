60222baac00c3dca0496577cf85dbdfef4d00bbf
Igor K <russdreamer@gmail.com>
2019-09-13 17:49:01 +0300

    BulkHead template

--- movie-catalog-service/src/main/java/com/toolittlespot/moviecatalogservice/services/MovieInfo.java
+++ movie-catalog-service/src/main/java/com/toolittlespot/moviecatalogservice/services/MovieInfo.java
@@ -1,6 +1,7 @@
 package com.toolittlespot.moviecatalogservice.services;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.toolittlespot.moviecatalogservice.models.CatalogItem;
 import com.toolittlespot.moviecatalogservice.models.Movie;
 import com.toolittlespot.moviecatalogservice.models.Rating;
@@ -14,7 +15,12 @@ public class MovieInfo {
     @Autowired
     RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem",
+    threadPoolKey = "movieInfoPool",
+    threadPoolProperties = {
+            @HystrixProperty(name = "coreSize", value = "20"),
+            @HystrixProperty(name = "maxQueueSize", value = "10")
+    })
     public CatalogItem getCatalogItem(Rating rating) {
         Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
         return new CatalogItem(movie.getMovieName(), movie.getMovieDescription(), rating.getMovieRating());
